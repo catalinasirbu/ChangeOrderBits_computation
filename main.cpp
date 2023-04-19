33d6ca9b7f5deb394f8843bf21eb7913a273db43
catalinasirbu <catalinageorgiana9428@gmail.com>
2023-04-19 18:57:02 +0300

    Computation modified

--- main.cpp
+++ main.cpp
@@ -2,17 +2,10 @@
 // https://github.com/catalinasirbu/ChangeOrderBits_computation.git
 
 // Output:
+// Ran in 873032 microseconds = 0.8 seconds
 
 // CPU used: Intel(R) Core(TM) i7-6700HQ CPU @ 2.60GHz
 // gcc (x86_64-posix-seh-rev2, Built by MinGW-W64 project) 12.2.0
-
-/* Initial result
- * Started
- * Init done
- * 00000000000000000000000000000011
- * 10000000000000001000000000000000
- * Ran in 3836 ms
-*/
 
 #include <iostream>
 #include <chrono>
@@ -21,46 +14,37 @@
 #define VECTOR_SIZE (100*1000*1000UL)
 uint32_t t[VECTOR_SIZE];
 
-#define BITARR(i, y) man |= ((u >> i) & 1) << (y - (i / 2));
-#define BITARR2(i) BITARR(i, 31);BITARR(i+1, 15);
-#define BITARR4(i) BITARR2(i); BITARR2(i+2);
-#define BITARR8(i) BITARR4(i); BITARR4(i+4);
-
 static inline void swapu32(uint32_t& u) {
-    uint32_t man = 0;
-    BITARR8(0);
-    BITARR8(8);
-    BITARR8(16);
-    BITARR8(24);
-    u = man;
+    u = ((u & 0x55555555) << 1) | ((u & 0xAAAAAAAA) >> 1);
+    u = ((u & 0x33333333) << 2) | ((u & 0xCCCCCCCC) >> 2);
+    u = ((u & 0x0F0F0F0F) << 4) | ((u & 0xF0F0F0F0) >> 4);
+    u = ((u & 0x00FF00FF) << 8) | ((u & 0xFF00FF00) >> 8);
+    u = ((u & 0x0000FFFF) << 16) | ((u & 0xFFFF0000) >> 16);
 }
 
 void init() {
-    for (uint32_t i = 0; i < VECTOR_SIZE; i++)
+    for (uint32_t i = 0; i < VECTOR_SIZE; i++) {
         t[i] = rand();
+    }
 }
 
-using namespace std::chrono;
 uint64_t getMilliseconds() {
-    auto now = system_clock::now();
-    auto millis = duration_cast<milliseconds>
-            (now.time_since_epoch()).count();
-    return millis;
+    return std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::high_resolution_clock::now().time_since_epoch())
+            .count();
 }
 
-uint8_t lut8[256];
 void init_lut8() {
+    uint8_t lut8[256] = {0};
     for (int i = 0; i < 256; i++) {
-        lut8[i] = 0;
-        lut8[i] |= ((i & 0x01) >> 0) << 7; // bit0
-        lut8[i] |= ((i & 0x04) >> 2) << 6; // bit2
-        lut8[i] |= ((i & 0x10) >> 4) << 5; // bit4
-        lut8[i] |= ((i & 0x40) >> 6) << 4; // bit6
-
-        lut8[i] |= ((i & 0x02) >> 1) << 3; // bit1
-        lut8[i] |= ((i & 0x08) >> 3) << 2; // bit3
-        lut8[i] |= ((i & 0x20) >> 5) << 1; // bit5
-        lut8[i] |= ((i & 0x80) >> 7) << 0; // bit7
+        lut8[i] |= ((i & 0x01) << 7); // bit0
+        lut8[i] |= ((i & 0x02) << 5); // bit1
+        lut8[i] |= ((i & 0x04) << 3); // bit2
+        lut8[i] |= ((i & 0x08) << 1); // bit3
+        lut8[i] |= ((i & 0x10) >> 1); // bit4
+        lut8[i] |= ((i & 0x20) >> 3); // bit5
+        lut8[i] |= ((i & 0x40) >> 5); // bit6
+        lut8[i] |= ((i & 0x80) >> 7); // bit7
     }
 }
 
@@ -73,15 +57,15 @@ int main() {
     std::bitset<32> bs1(t[0]);
 
     auto tstart = getMilliseconds();
-    for (uint32_t i = 0; i < VECTOR_SIZE; i++)
+    for (uint32_t i = 0; i < VECTOR_SIZE; i++) {
         swapu32(t[i]);
+    }
     auto tstop = getMilliseconds();
 
-    //t[0] = swapu32(t[0]);
     std::bitset<32> bs2(t[0]);
     std::cout << bs1 << std::endl;
     std::cout << bs2 << std::endl;
 
-    std::cout << "Ran in " << (tstop - tstart) << " ms"  << std::endl;
+    std::cout << "Ran in " << (tstop - tstart) << " microseconds"  << std::endl;
     return 0;
 }
